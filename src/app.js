d167825b5a07e9c519a284fed9622b7fbaf7a396
pedroddomingues <pedroddomingues@gmail.com>
2020-07-10 21:59:05 -0300

    Desafio completo

--- src/app.js
+++ src/app.js
@@ -28,19 +28,40 @@ app.post("/repositories", (request, response) => {
   return response.json(repository)
 });
 
+app.use("/repositories/:id", (request, response, next) => {
+  const { id } = request.params
+
+  const repositoryIndex = repositories.findIndex(repo => repo.id == id)
+
+  if (repositoryIndex < 0) {
+    return response.status(400).json({error: "ID not found"})
+  }
+
+  request.repoI = repositoryIndex
+
+  return next()
+})
+
 app.put("/repositories/:id", (request, response) => {
-  const { id } = request.params;
   const { title, url, techs } = request.body;
-  
-//   repositories.findIndex(
+
+  repositories[request.repoI] = { ...repositories[request.repoI], title, url, techs }
+
+  return response.json(repositories[request.repoI])
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  repositories.splice(request.repoI,1)
+  return response.status(204).json({message: "Repo deleted."})
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  let {likes} = repositories[request.repoI]
+  likes = ++likes
+
+  repositories[request.repoI] = {...repositories[request.repoI], likes}
+
+  return response.json(repositories[request.repoI])
 });
 
 module.exports = app;
